45f5dd13a464421ad57de8252cb15e241195e448
Vidal <thomas-vidal@hotmail.com>
2015-11-23 20:29:02 +0100

    Add models

---
+++ Sms/Models/Dictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Sms.Models
+{
+    public class Dictionary
+    {
+
+        private List<Item> items;
+
+        public Dictionary()
+        {
+            items = new List<Item>();
+        }
+
+        [XmlArray("Dictionary"), XmlArrayItem(typeof(Item), ElementName = "Item")]
+        public List<Item> Items
+        {
+            get
+            {
+                return items;
+            }
+
+            set
+            {
+                items = value;
+            }
+        }
+    }
+}
---
+++ Sms/Models/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Sms.Models
+{
+    [Serializable]
+    public class Item
+    {
+        private string term;
+        private string word;
+
+        [XmlAttribute()]
+        public string Term
+        {
+            get
+            {
+                return term;
+            }
+
+            set
+            {
+                term = value;
+            }
+        }
+
+        [XmlAttribute()]
+        public string Word
+        {
+            get
+            {
+                return word;
+            }
+
+            set
+            {
+                word = value;
+            }
+        }
+    }
+}
